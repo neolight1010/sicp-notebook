6e593936a5e1a8dbacb9ab006f53b263ddb76903
Anthony Suárez <neolight1010@gmail.com>
2026-04-26 12:04:55 -0500

    Delete clojure files

--- test/sidc_notebook/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns sidc-notebook.core-test
-  (:require [clojure.test :refer :all]
-            [sidc-notebook.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
