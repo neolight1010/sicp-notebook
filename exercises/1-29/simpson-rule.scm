a0fa2eba67f435bbbfe7d0b39c1237060e58c74b
Anthony Suárez <neolight1010@gmail.com>
2026-06-14 18:47:51 -0500

    fix exercise 1.29

--- exercises/1-29/simpson-rule.scm
+++ exercises/1-29/simpson-rule.scm
@@ -1,27 +1,36 @@
 (define (simpson f a b n)
   (define (h)
     (/ (- b a) n))
-  
-  (* (/ (h) 3) (simpson-iter f a n n (h))))
 
+  (define (y k)
+    (f (+ a (* k (h)))))
 
-(define (simpson-iter f a n k h)
-  (define (y)
-    (f (+ a (* k h))))
+  (define (term k)
+    (cond
+      ((= k 0) (y 0))
+      ((= k n) (y n))
+      ((even? k) (* 2 (y k)))
+      (else (* 4 (y k)))))
 
-  (define (next)
-    (simpson-iter f a n (- k 1) h))
-  
-  (cond
-    ((= k n) (+ (y) (next)))
-    ((= k 0) (y))
-    ((even? k) (+ (* 2 (y)) (next)))
-    (else (+ (* 4 (y)) (next)))))
+  (* (/ (h) 3) (sum term a inc n)))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a) (sum term (next a) next b))))
+
+(define (inc a) (+ a 1))
+
+; tests
 
 (define (cube n)
   (* n n n))
 
-(print (simpson cube 0 1.0 100))
-(print (simpson cube 0 1.0 1000))
+(define (weird x)
+  (+ (* x (- x 2)) (- x 5)))
 
-(print (simpson cube 0 20. 100))
+(print (simpson cube 0 1.0 100)) ; 0.25
+(print (simpson cube 0 1.0 1000)) ; 0.25
+(print (simpson cube 0 20. 100)) ; 40000
+
+(print (simpson weird 0 20. 100)) ; ~2366.7
