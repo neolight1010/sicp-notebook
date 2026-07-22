a43dcd8ebf23617ce0a27b86d51c431fe2bb65d7
Anthony Suárez <neolight1010@gmail.com>
2026-07-21 21:20:24 -0500

    exercise 1.37

---
+++ exercises/1-37/continued-fraction.scm
@@ -0,0 +1,32 @@
+; recursive
+(define (cont-frac n d k)
+  (define (cont-frac-iter i)
+    (if (= i k)
+        (/ (n k) (d k))
+        (/ (n i) (+ (d i) (cont-frac-iter (+ i 1))))))
+  
+  (cont-frac-iter 1))
+
+(print "recursive:")
+(print (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 12))
+
+(newline)
+
+; iterative
+(define (cont-frac n d k)
+  (define (cont-frac-iter i prev-q)
+    (let ((q (/ (n (- i 1)) (+ (d (- i 1)) prev-q))
+             ))
+      (if (= i 1)
+          prev-q
+          (cont-frac-iter (- i 1) q))))
+  
+  (cont-frac-iter k (/ (n k) (d k))))
+
+(print "iterative:")
+(print (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 12))
+
+; 4 decimal places is 0.6180
+
+; k=11 yields 0.618055555555556, which is rounded to 0.6181
+; k=12 yields 0.618025751072961, which is rounded to 0.6180
